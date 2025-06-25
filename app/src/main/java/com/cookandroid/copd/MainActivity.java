18a7b24446e012c9808d6fb7ddd3c19c4ac732f8
YEJIN <yejin1228@pukyong.ac.kr>
2025-06-25 17:49:54 +0900

    초기 커밋

---
+++ app/src/main/java/com/cookandroid/copd/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cookandroid.copd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button trainingBtn = findViewById(R.id.Training);
+
+        trainingBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, trainingActivity.class);
+            startActivity(intent);
+        });
+        }
+
+    }
+
+
---
+++ app/src/main/java/com/cookandroid/copd/trainingActivity.java
@@ -0,0 +1,11 @@
+package com.cookandroid.copd;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class trainingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.training);
+    }
+}
