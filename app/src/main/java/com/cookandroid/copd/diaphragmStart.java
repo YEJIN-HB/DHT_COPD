ed92367edd394e3b4f4ff17d77498a4c7d77999a
YEJIN <yejin1228@pukyong.ac.kr>
2025-06-26 11:44:30 +0900

    호흡훈련 기능 제작완료

--- app/src/main/java/com/cookandroid/copd/diaphragmStart.java
+++ app/src/main/java/com/cookandroid/copd/diaphragmStart.java
@@ -1,80 +1,139 @@
 package com.cookandroid.copd;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.app.Activity;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.os.Handler;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class diaphragmStart extends Activity {
-    int completeCount = 0;
+
+    Button btnStartTimer5, btnStartTimer10, btnEnd, btnHome;
+    Chronometer chronometer;
+    TextView textCount;
+    WebView webView;
+    int count = 0;
+
+    String beforeCondition = "";
+    String afterCondition = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diaphragm_start);
 
-        WebView webView = findViewById(R.id.D_Vedio);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        btnStartTimer5 = findViewById(R.id.btnStartTimer5);
+        btnStartTimer10 = findViewById(R.id.btnStartTimer10);
+        btnEnd = findViewById(R.id.btnEnd);
+        btnHome = findViewById(R.id.btnHome);
+        chronometer = findViewById(R.id.D_timer);
+        textCount = findViewById(R.id.textCount);
+        webView = findViewById(R.id.D_Vedio);
 
-        String videoId = "wD-D7VKglJg";
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+// 유튜브 영상 ID만 추출해서 iframe에 삽입
+        String videoId = "1QjvUrS5r4k";
         String html = "<html><body style='margin:0;padding:0;'>" +
-                "<iframe width='100%' height='200' src='https://www.youtube.com/embed/" +
+                "<iframe width='100%' height='100%' src='https://www.youtube.com/embed/" +
                 videoId + "' frameborder='0' allowfullscreen></iframe>" +
                 "</body></html>";
 
         webView.loadDataWithBaseURL(null, html, "text/html", "utf-8", null);
 
-        Chronometer chrono;
-        chrono = findViewById(R.id.D_timer);
-        Button btnStart5 = (Button) findViewById(R.id.btnStartTimer5);
-        btnStart5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
+        btnStartTimer5.setOnClickListener(v -> showBeforeDialog(5));
+        btnStartTimer10.setOnClickListener(v -> showBeforeDialog(10));
 
-                new Handler().postDelayed(() -> chrono.stop(), 5*60*1000);
-            }
+        btnEnd.setOnClickListener(v -> {
+            chronometer.stop();
+            count++;
+            textCount.setText("훈련 횟수: " + count + "회");
+            showAfterDialog();
         });
 
-        Button btnStart10 = (Button) findViewById(R.id.btnStartTimer10);
-        btnStart10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-
-                new Handler().postDelayed(() -> chrono.stop(), 10*60*1000);
-            }
-        });
-
-
-        TextView textCount = findViewById(R.id.textCount);
-        Button btnCount = (Button) findViewById(R.id.btnEnd);
-        btnCount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                completeCount++;
-                textCount.setText("완료 횟수: " + completeCount + "회");
-            }
-        });
-
-        Button btnHome = (Button) findViewById(R.id.btnHome);
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),
-                        MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        btnHome.setOnClickListener(v -> finish());
     }
 
+    private void showBeforeDialog(int minutes) {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_status, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("훈련 전 상태 입력");
+        builder.setView(dialogView);
+
+        RadioGroup radioBreath = dialogView.findViewById(R.id.radioBreath);
+        RadioGroup radioMood = dialogView.findViewById(R.id.radioMood);
+
+        builder.setPositiveButton("확인", (dialog, which) -> {
+            int breathId = radioBreath.getCheckedRadioButtonId();
+            int moodId = radioMood.getCheckedRadioButtonId();
+
+            if (breathId != -1 && moodId != -1) {
+                RadioButton breathSelected = dialogView.findViewById(breathId);
+                RadioButton moodSelected = dialogView.findViewById(moodId);
+
+                beforeCondition = breathSelected.getText().toString() + " / " + moodSelected.getText().toString();
+                Toast.makeText(getApplicationContext(), "훈련을 시작합니다.", Toast.LENGTH_SHORT).show();
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+            } else {
+                Toast.makeText(getApplicationContext(), "모든 항목을 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("취소", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
+
+    private void showAfterDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_status, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("훈련 후 상태 입력");
+        builder.setView(dialogView);
+
+        RadioGroup radioBreath = dialogView.findViewById(R.id.radioBreath);
+        RadioGroup radioMood = dialogView.findViewById(R.id.radioMood);
+
+        builder.setPositiveButton("확인", (dialog, which) -> {
+            int breathId = radioBreath.getCheckedRadioButtonId();
+            int moodId = radioMood.getCheckedRadioButtonId();
+
+            if (breathId != -1 && moodId != -1) {
+                RadioButton breathSelected = dialogView.findViewById(breathId);
+                RadioButton moodSelected = dialogView.findViewById(moodId);
+
+                afterCondition = breathSelected.getText().toString() + " / " + moodSelected.getText().toString();
+
+                String summary = "훈련 전: " + beforeCondition + "\n"
+                        + "훈련 후: " + afterCondition;
+
+                new AlertDialog.Builder(this)
+                        .setTitle("훈련 결과 요약")
+                        .setMessage(summary)
+                        .setPositiveButton("확인", null)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(), "모든 항목을 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("취소", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
 }
--- app/src/main/java/com/cookandroid/copd/dtStart.java
+++ app/src/main/java/com/cookandroid/copd/dtStart.java
@@ -1,67 +1,122 @@
 package com.cookandroid.copd;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.app.Activity;
+import android.widget.Toast;
 
 public class dtStart extends Activity {
     int completeCount = 0;
+    String beforeCondition = "";
+    String afterCondition = "";
+    Chronometer chrono;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lung_start);
 
-        Chronometer chrono;
         chrono = findViewById(R.id.D_timer);
-        Button btnStart5 = (Button) findViewById(R.id.btnStartTimer5);
-        btnStart5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-
-                new Handler().postDelayed(() -> chrono.stop(), 5*60*1000);
-            }
-        });
-
-        Button btnStart10 = (Button) findViewById(R.id.btnStartTimer10);
-        btnStart10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-
-                new Handler().postDelayed(() -> chrono.stop(), 10*60*1000);
-            }
-        });
-
-
         TextView textCount = findViewById(R.id.textCount);
-        Button btnCount = (Button) findViewById(R.id.btnEnd);
-        btnCount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                completeCount++;
-                textCount.setText("완료 횟수: " + completeCount + "회");
-            }
+        Button btnStart5 = findViewById(R.id.btnStartTimer5);
+        Button btnStart10 = findViewById(R.id.btnStartTimer10);
+        Button btnCount = findViewById(R.id.btnEnd);
+        Button btnHome = findViewById(R.id.btnHome);
+
+        btnStart5.setOnClickListener(v -> showBeforeDialog(5));
+        btnStart10.setOnClickListener(v -> showBeforeDialog(10));
+
+        btnCount.setOnClickListener(v -> {
+            chrono.stop();
+            completeCount++;
+            textCount.setText("완료 횟수: " + completeCount + "회");
+            showAfterDialog();
         });
 
-        Button btnHome = (Button) findViewById(R.id.btnHome);
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),
-                        MainActivity.class);
-                startActivity(intent);
-            }
+        btnHome.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
         });
     }
 
-}
+    private void showBeforeDialog(int minutes) {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_status, null);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("훈련 전 상태 입력");
+        builder.setView(dialogView);
+
+        RadioGroup radioBreath = dialogView.findViewById(R.id.radioBreath);
+        RadioGroup radioMood = dialogView.findViewById(R.id.radioMood);
+
+        builder.setPositiveButton("확인", (dialog, which) -> {
+            int breathId = radioBreath.getCheckedRadioButtonId();
+            int moodId = radioMood.getCheckedRadioButtonId();
+
+            if (breathId != -1 && moodId != -1) {
+                RadioButton breathSelected = dialogView.findViewById(breathId);
+                RadioButton moodSelected = dialogView.findViewById(moodId);
+
+                beforeCondition = breathSelected.getText().toString() + " / " + moodSelected.getText().toString();
+                Toast.makeText(getApplicationContext(), "훈련을 시작합니다.", Toast.LENGTH_SHORT).show();
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+
+                new Handler().postDelayed(() -> chrono.stop(), minutes * 60 * 1000);
+            } else {
+                Toast.makeText(getApplicationContext(), "모든 항목을 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("취소", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
+
+    private void showAfterDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_status, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("훈련 후 상태 입력");
+        builder.setView(dialogView);
+
+        RadioGroup radioBreath = dialogView.findViewById(R.id.radioBreath);
+        RadioGroup radioMood = dialogView.findViewById(R.id.radioMood);
+
+        builder.setPositiveButton("확인", (dialog, which) -> {
+            int breathId = radioBreath.getCheckedRadioButtonId();
+            int moodId = radioMood.getCheckedRadioButtonId();
+
+            if (breathId != -1 && moodId != -1) {
+                RadioButton breathSelected = dialogView.findViewById(breathId);
+                RadioButton moodSelected = dialogView.findViewById(moodId);
+
+                afterCondition = breathSelected.getText().toString() + " / " + moodSelected.getText().toString();
+
+                String summary = "훈련 전: " + beforeCondition + "\n훈련 후: " + afterCondition;
+
+                new AlertDialog.Builder(this)
+                        .setTitle("훈련 결과 요약")
+                        .setMessage(summary)
+                        .setPositiveButton("확인", null)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(), "모든 항목을 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("취소", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
+}
